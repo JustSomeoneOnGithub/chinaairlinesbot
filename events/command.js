b71deb5cb28d0159ff1e75f9823147e81f2b5c4f
JustSomeoneOnGithub <59625582+JustSomeoneOnGithub@users.noreply.github.com>
2020-05-19 19:24:02 -0400

    Add files via upload

---
+++ events/command.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+module.exports = {
+    name: '',
+    description: "",
+    execute(message, args){
+       // Insert Code Here.
+    }
+}
